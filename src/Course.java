474fec975ee024c0f46165a33688e73db51fbac5
schindlerjake <jschind@uwo.ca>
2017-02-15 19:47:03 -0500

    Added course class and made changes to Student and Instructor

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by jakeschindler on 2017-02-15.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String course, String description){
+        this.cname = course;
+        this.description = description;
+    }
+}
--- src/Student.java
+++ src/Student.java
@@ -1,15 +1,25 @@
+import java.util.List;
+
 /**
  * Created by ethan on 2017-02-07.
  */
 public class Student extends Person {
     String major;
+    List<Course> courseList;
+    boolean fulltime;
 
-    public Student(String name, String emailAddress, String major) {
+    public Student(String name, String emailAddress, String major, boolean fulltime) {
         super(name, emailAddress);
         this.major = major;
+        this.fulltime =fulltime;
     }
 
     public String toString() {
         return String.format("Name: %s\nMajor: %s\nEmail: %s", this.name, this.major, this.emailAddress);
+    }
+
+    public void addCourse(Course c){
+        if (!courseList.contains(c))
+            courseList.add(c);
     }
 }
